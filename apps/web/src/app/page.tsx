209b3c17105f04cdd1be870d009509361e6cb4d2
akshatarora130 <akshatarora130@gmail.com>
2024-05-10 00:18:22 +0530

    web socket server established

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+const Page = (): JSX.Element => {
+  return (
+      <>
+
+      </>
+  );
+}
+
+export default Page
---
+++ apps/ws/src/GameManager.ts
@@ -0,0 +1,47 @@
+import WebSocket from "ws";
+import {Game} from "./Game";
+import {INIT_GAME, MOVE} from "./message";
+
+export class GameManager{
+    private games: Game[];
+    private pendingUser: WebSocket | null
+    private users: WebSocket[]
+
+    constructor() {
+        this.games = []
+        this.users = []
+        this.pendingUser = null
+    }
+
+    addUser(socket: WebSocket){
+        this.users.push(socket);
+        this.addHandler(socket);
+    }
+
+    removeUser(socket: WebSocket){
+        this.users = this.users.filter((user) => user !== socket);
+    }
+
+    private addHandler (socket: WebSocket){
+        socket.on("message", (data) => {
+            const message = JSON.parse(data.toString());
+
+            if(message.type == INIT_GAME){
+                if(this.pendingUser){
+                    const game = new Game(this.pendingUser, socket);
+                    this.games.push(game);
+                    this.pendingUser = null;
+                }else{
+                    this.pendingUser = socket
+                }
+            }
+
+            if (message.type == MOVE){
+                const game = this.games.find((g) => g.player1 == socket || g.player2 == socket);
+                if(game){
+                    game.makeMove(socket, message.move);
+                }
+            }
+        })
+    }
+}
